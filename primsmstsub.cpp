8192d6079f8fe8f8f9fc91532c4ff920053ac513
Chakshu Ahuja <ahuja.chaks@gmail.com>
2016-03-07 01:02:51 +0530

    Minimum Spanning tree using Prims

--- primsmstsub.cpp
+++ primsmstsub.cpp
@@ -3,8 +3,10 @@ int graph[3000][3000];
 int mstSet[3000];
 int N, M;
 int dist[3000];
+#define INF 99999;
 int minimum_dist() {
-  int minimum = INF, min_index = -1;
+  int minimum = INF;
+  int min_index = -1;
   for(int i = 0; i < N; i++) {
     if(!mstSet[i] && dist[i] < minimum) {
       minimum = dist[i];
@@ -18,12 +20,22 @@ void prims() {
     mstSet[i] = false;
     dist[i] = INF;
   }
-  mstSet[0] = true;
   dist[0] = 0;
-  int u = minimum_dist();
-  while(u != -1) {
-    
+  
+  for(int count = 0; count < N-1; count++) {
+    int u = minimum_dist();
+    mstSet[u] = true;
+    for(int v = 0; v < N; v++) {
+      if(graph[u][v] != -1 && !mstSet[v] && dist[v] > graph[u][v]) {
+	dist[v] = graph[u][v];
+      }
+    }
   }
+  int sum = 0;
+  for(int i = 0; i < N; i++) {
+    sum += dist[i];
+  }
+  std::cout << sum << std::endl;
 }
 int main() {
   std::cin >> N >> M;
@@ -35,8 +47,10 @@ int main() {
   for(int i = 0; i < M; i++) {
     int x, y, r;
     std::cin >> x >> y >> r;
-    graph[x][y] = r;
-    graph[y][x] = r;
+    graph[x-1][y-1] = r;
+    graph[y-1][x-1] = r;
   }
+  int src;
+  std::cin >> src;
   prims();
 }
