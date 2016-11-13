6b5b288474dcf8bb0be4b86813f379a2a08b979a
Chakshu Ahuja <ahuja.chaks@gmail.com>
2016-11-12 16:21:19 -0800

    Jessie Loves Candy :  Adobe CODHERS Codesprint

---
+++ jessie_loves_candy.py
@@ -0,0 +1,8 @@
+#  Adobe CODHERS Codesprint
+# Problem Statement : https://www.hackerrank.com/contests/adobe-codhers/challenges/jesse-loves-candy
+
+N, k = map(int, raw_input().split())
+b = map(int, raw_input().split())
+candies = [i for i in range(N) if b[i] == 1]
+diff = [t - s for s, t in zip(candies, candies[1:])]
+print min(candies[i]+k*diff[i] for i in range(len(diff)))
