4742dfee702113d8d0ad81840998e263ecceb59c
Chakshu Ahuja <ahuja.chaks@gmail.com>
2016-11-13 16:39:27 -0800

    Jessie Loves Candy: Adobe CODHERS Codesprint

--- jessie_loves_candy.py
+++ jessie_loves_candy.py
@@ -1,6 +1,6 @@
 #  Adobe CODHERS Codesprint
 # Problem Statement : https://www.hackerrank.com/contests/adobe-codhers/challenges/jesse-loves-candy
-
+# TIP: Do not assume that taking candy from first shop will be the fastest
 N, k = map(int, raw_input().split())
 b = map(int, raw_input().split())
 candies = [i for i in range(N) if b[i] == 1]
